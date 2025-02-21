c0f9f3e27e0ccae7a454ce40e96084684515bcd2
MoniLopez <mon.lopmar@gmail.com>
2025-02-21 17:39:32 -0600

    Se crean funciones para agregar y sortear amigos

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []; //Almacena los nombres ingresados
+
+function agregarAmigo(){
+let nombreInput = document.getElementById('amigo'); //Captura campo de entrada document.getElementByIdlll
+let nombre = nombreInput.value;
+console.log(nombre);
+if (nombre == ""){//Valida entrada, que no esté vacío
+    alert("Por favor, inserte un nombre");
+}
+else{
+    amigos.push(nombre);//Agrega valor al array
+    nombreInput.value = ''; //Limpia campo de entrada con cadena vacía
+}
+
+}
+
+function sortearAmigo(){
+//Valida que el array amigos no esté vacío
+if (amigos.length != 0){
+    let indice = Math.floor(Math.random()*amigos.length); //Genera índice aleatorio
+    let amigoSecreto = amigos[indice]; //Obtener nombre sorteado
+    //Muestra el resultado
+    console.log(amigoSecreto);
+}else{
+    alert("No hay amigos para sortear");
+}
+}
